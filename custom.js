787988975754f9ebd15197815a3b384273261bbb
AlyRizuu <84929790+AlyRizuu@users.noreply.github.com>
2023-01-13 16:57:31 +0500

    Third commit

---
+++ custom.js
@@ -0,0 +1,24 @@
+
+
+// code of owl-carousel
+
+$(".carousel").owlCarousel({
+		margin: 10,
+		loop: true,
+		responsive:{
+			0:{
+				items:1,
+				nav:false,
+			},
+			600:{
+				items:2,
+				nav:false,
+			},
+			1000:{
+				items:3,
+				nav:false,
+			},
+		}
+	}
+);
+
